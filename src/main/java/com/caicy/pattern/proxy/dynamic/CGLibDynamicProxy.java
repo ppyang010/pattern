7d84b4ff6520772ea8581fe763a7c7b5bae15fb8
CCY <644724460@qq.com>
2019-07-17 00:54:31 +0800

    代码优化

--- src/main/java/com/caicy/pattern/proxy/dynamic/CGLibDynamicProxy.java
+++ src/main/java/com/caicy/pattern/proxy/dynamic/CGLibDynamicProxy.java
@@ -9,14 +9,14 @@ import java.lang.reflect.Method;
 /**
  * Created by Administrator on 2018/3/18.
  */
-public class CGLibDynamicProxy implements MethodInterceptor{
+public class CGLibDynamicProxy implements MethodInterceptor {
 
 
-    public <T> T getProxy(Class<T> cls){
+    public <T> T getProxy(Class<T> cls) {
         //创建代理类
         //参数1 被代理类 的class
         //参数2 MethodInterceptor 的实现类
-        return (T) Enhancer.create(cls,this);
+        return (T) Enhancer.create(cls, this);
     }
 
     /**
@@ -27,34 +27,35 @@ public class CGLibDynamicProxy implements MethodInterceptor{
      * @param MethodProxy 表示对方法的代理，invokeSuper方法表示对被代理对象方法的调用
      */
     /**
-     Class clazzB = B.class;
-     Class clazzD = D.class;
-     boolean isFather = clazzD.isAssignableFrom(clazzB));
-     if(isFather){
-     System.out.println("D是B的父类");
-     }else{
-     System.out.println("D不是B的父类");
-     }
+     * Class clazzB = B.class;
+     * Class clazzD = D.class;
+     * boolean isFather = clazzD.isAssignableFrom(clazzB));
+     * if(isFather){
+     * System.out.println("D是B的父类");
+     * }else{
+     * System.out.println("D不是B的父类");
+     * }
      */
     @Override
     public Object intercept(Object obj, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
-        if(method.getName().equals("rentOut")){
+        if (method.getName().equals("rentOut")) {
             beforeRent();
         }
         System.out.println(obj.getClass().toString());
         System.out.println(methodProxy.getClass().toString());
-        Object res = methodProxy.invokeSuper(obj,args);
-        if(method.getName().equals("rentOut")){
+        Object res = methodProxy.invokeSuper(obj, args);
+        if (method.getName().equals("rentOut")) {
             afterRent();
         }
         return res;
     }
+
     //这里的before after 方法可以提取出去
-    public void beforeRent(){
+    public void beforeRent() {
         System.out.println("确定租期，房租，押金");
     }
 
-    public void afterRent(){
+    public void afterRent() {
         System.out.println("拿到钥匙");
     }
 }
